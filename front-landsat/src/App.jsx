c00315d0a7d744b119d53f36bf15e4cccb4da5bd
vladimir.copa <vladimir.copa@agetic.gob.bo>
2024-10-06 09:03:06 -0400

    fix: correccion de fondo de la pantalla

--- front-landsat/src/App.jsx
+++ front-landsat/src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import LocationInput from "./components/Prueba";
-import LocationSelector from "./pages/maps/Mapa";
 import Cordenadas from "./pages/maps/cordenadas";
 import SpectralSignatureChart from "./components/Camb";
 import NavBar from "./components/NavBar";
@@ -12,19 +11,20 @@ import { Container, Toolbar } from "@mui/material";
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Container maxWidth="xl">
-          <NavBar />
-          <Toolbar />
-          <Routes>
-            <Route path="/hero" element={<Home />} />
-            <Route path="/" element={<Hero />} />
-            <Route path="/location-selector" element={<Cordenadas />} />
-            <Route path="/spectral-signature" element={<SpectralSignatureChart />} />
-            <Route path="/location-input" element={<LocationInput />} />
-          </Routes>
-        </Container>
-      </div>
+      <Container maxWidth="xl">
+        <NavBar />
+        <Toolbar />
+        <Routes>
+          <Route path="/hero" element={<Home />} />
+          <Route path="/" element={<Hero />} />
+          <Route path="/location-selector" element={<Cordenadas />} />
+          <Route
+            path="/spectral-signature"
+            element={<SpectralSignatureChart />}
+          />
+          <Route path="/location-input" element={<LocationInput />} />
+        </Routes>
+      </Container>
     </Router>
   );
 }
